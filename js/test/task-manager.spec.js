403428d7b96fb1d89171eca4075b2ea12a1422c5
James Hines <jameshines10@gmail.com>
2015-06-20 17:32:39 -0400

    Sets up unit tests to run using Jasmine Standalone

--- js/test/task-manager.spec.js
+++ js/test/task-manager.spec.js
@@ -1,3 +1,18 @@
 /**
  * Created by james on 6/20/15.
  */
+var _hello = null;
+
+beforeEach(function(done){
+    require(['../js/src/task-manager.js'], function(hello){
+        _hello = hello;
+
+        done();
+    });
+});
+
+describe("the hello class", function () {
+    it("should say hello", function() {
+        expect(_hello.sayHello()).toEqual("Hello World!");
+    });
+});
