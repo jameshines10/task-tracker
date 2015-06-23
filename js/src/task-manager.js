fc84f61270fd54c5cfdc673f4e2040121ee947df
James Hines <jameshines10@gmail.com>
2015-06-23 00:38:55 -0400

    Refactors the methods for loading and saving tasks

--- js/src/task-manager.js
+++ js/src/task-manager.js
@@ -13,26 +13,32 @@ define(function(){
         }
 
         if (_localStorage) {
-            _tasks = _localStorage.getItem('task-manager-tasks');
+            _tasks = JSON.parse(_localStorage.getItem('task-manager-tasks'));
 
             if(_tasks.length === 0) {
                 _tasks = [
-                    {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
-                    {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
-                    {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
-                    {"name": "Test Task #4", "date": "12/04/2012", "assigned": "John Doe" },
-                    {"name": "Test Task #5", "date": "12/05/2012", "assigned": "John Doe" },
-                    {"name": "Test Task #6", "date": "12/06/2012", "assigned": "John Doe" },
-                    {"name": "Test Task #7", "date": "12/07/2012", "assigned": "John Doe" }
+                    {'name': 'Test Task #1', 'date': '12/01/2012', 'assigned': 'John Doe' },
+                    {'name': 'Test Task #2', 'date': '12/02/2012', 'assigned': 'John Doe' },
+                    {'name': 'Test Task #3', 'date': '12/03/2012', 'assigned': 'John Doe' },
+                    {'name': 'Test Task #4', 'date': '12/04/2012', 'assigned': 'John Doe' },
+                    {'name': 'Test Task #5', 'date': '12/05/2012', 'assigned': 'John Doe' },
+                    {'name': 'Test Task #6', 'date': '12/06/2012', 'assigned': 'John Doe' },
+                    {'name': 'Test Task #7', 'date': '12/07/2012', 'assigned': 'John Doe' }
                 ];
 
-                _localStorage.setItem('task-manager-tasks', _tasks);
+                _localStorage.setItem('task-manager-tasks', JSON.stringify(_tasks));
             }
         }
     };
 
     TaskManager.prototype.getTasks = function() {
         return _tasks;
+    };
+
+    TaskManager.prototype.saveTask = function(task) {
+        _tasks.push(task);
+
+        _localStorage.setItem('task-manager-tasks', JSON.stringify(_tasks));
     };
 
     return TaskManager;
--- js/test/task-manager.spec.js
+++ js/test/task-manager.spec.js
@@ -15,59 +15,59 @@ beforeEach(function(done){
 beforeEach(function () {
     store = {
         'task-manager-tasks': [
-            {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
-            {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
-            {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" }
+            {'name': 'Test Task #1', 'date': '12/01/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #2', 'date': '12/02/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #3', 'date': '12/03/2012', 'assigned': 'John Doe' }
         ]
     };
 
     spyOn(localStorage, 'getItem').and.callFake(function () {
-        return store['task-manager-tasks'];
+        return JSON.stringify(store['task-manager-tasks']);
     });
 
     spyOn(localStorage, 'setItem').and.callFake(function () {
-        return store['task-manager-tasks'] = [
-            {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
-            {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
-            {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
-            {"name": "Test Task #4", "date": "12/04/2012", "assigned": "John Doe" },
-            {"name": "Test Task #5", "date": "12/05/2012", "assigned": "John Doe" },
-            {"name": "Test Task #6", "date": "12/06/2012", "assigned": "John Doe" },
-            {"name": "Test Task #7", "date": "12/07/2012", "assigned": "John Doe" }
-        ];
+        return store['task-manager-tasks'] = JSON.stringify([
+            {'name': 'Test Task #1', 'date': '12/01/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #2', 'date': '12/02/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #3', 'date': '12/03/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #4', 'date': '12/04/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #5', 'date': '12/05/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #6', 'date': '12/06/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #7', 'date': '12/07/2012', 'assigned': 'John Doe' }
+        ]);
     });
 });
 
-describe("the TaskManager class", function () {
-    it("should not pollute the global scope", function() {
+describe('the TaskManager class', function () {
+    it('should not pollute the global scope', function() {
         _taskManager.loadTasks();
 
         expect(_taskManager._tasks).toEqual(undefined);
     });
 
-    it("should load a list of tasks", function() {
+    it('should load a list of tasks', function() {
         _taskManager.loadTasks();
 
         expect(_taskManager.getTasks()).toEqual([
-            {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
-            {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
-            {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" }
+            {'name': 'Test Task #1', 'date': '12/01/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #2', 'date': '12/02/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #3', 'date': '12/03/2012', 'assigned': 'John Doe' }
         ]);
     });
 
-    it("should populate the tasks array with tasks if there are none in localStorage", function(){
+    it('should populate the tasks array with tasks if there are none in localStorage', function(){
         store['task-manager-tasks'] = [];
 
         _taskManager.loadTasks();
 
         expect(_taskManager.getTasks()).toEqual([
-            {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
-            {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
-            {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
-            {"name": "Test Task #4", "date": "12/04/2012", "assigned": "John Doe" },
-            {"name": "Test Task #5", "date": "12/05/2012", "assigned": "John Doe" },
-            {"name": "Test Task #6", "date": "12/06/2012", "assigned": "John Doe" },
-            {"name": "Test Task #7", "date": "12/07/2012", "assigned": "John Doe" }
+            {'name': 'Test Task #1', 'date': '12/01/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #2', 'date': '12/02/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #3', 'date': '12/03/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #4', 'date': '12/04/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #5', 'date': '12/05/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #6', 'date': '12/06/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #7', 'date': '12/07/2012', 'assigned': 'John Doe' }
         ]);
     });
 
@@ -82,11 +82,13 @@ describe("the TaskManager class", function () {
 
         _taskManager.saveTask(task);
 
+        expect(localStorage.setItem).toHaveBeenCalled();
+
         expect(_taskManager.getTasks()).toEqual([
-            {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
-            {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
-            {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
-            {"name": "Test Task #100", "date": "06/23/2015", "assigned": "James Hines" }
+            {'name': 'Test Task #1', 'date': '12/01/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #2', 'date': '12/02/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #3', 'date': '12/03/2012', 'assigned': 'John Doe' },
+            {'name': 'Test Task #100', 'date': '06/23/2015', 'assigned': 'James Hines' }
         ])
     });
 });
