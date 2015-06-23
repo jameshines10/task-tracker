803c36c594aeeab4abd138ab12d4f2eebddb8673
James Hines <jameshines10@gmail.com>
2015-06-23 00:11:13 -0400

    Adds a failing test for persisting tasks

--- js/src/task-manager.js
+++ js/src/task-manager.js
@@ -13,20 +13,21 @@ define(function(){
         }
 
         if (_localStorage) {
-            _tasks = JSON.parse(_localStorage.getItem('task-manager-tasks')) || [];
+            _tasks = _localStorage.getItem('task-manager-tasks');
 
-        } else {
-            _tasks = [
-                {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
-                {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
-                {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
-                {"name": "Test Task #4", "date": "12/04/2012", "assigned": "John Doe" },
-                {"name": "Test Task #5", "date": "12/05/2012", "assigned": "John Doe" },
-                {"name": "Test Task #6", "date": "12/06/2012", "assigned": "John Doe" },
-                {"name": "Test Task #7", "date": "12/07/2012", "assigned": "John Doe" }
-            ];
+            if(_tasks.length === 0) {
+                _tasks = [
+                    {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
+                    {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
+                    {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
+                    {"name": "Test Task #4", "date": "12/04/2012", "assigned": "John Doe" },
+                    {"name": "Test Task #5", "date": "12/05/2012", "assigned": "John Doe" },
+                    {"name": "Test Task #6", "date": "12/06/2012", "assigned": "John Doe" },
+                    {"name": "Test Task #7", "date": "12/07/2012", "assigned": "John Doe" }
+                ];
 
-            _localStorage.setItem('task-manager-tasks', JSON.stringify(_tasks));
+                _localStorage.setItem('task-manager-tasks', _tasks);
+            }
         }
     };
 
--- js/test/task-manager.spec.js
+++ js/test/task-manager.spec.js
@@ -2,12 +2,39 @@
  * Created by james on 6/20/15.
  */
 var _taskManager = null;
+var store = {};
 
 beforeEach(function(done){
     require(['../js/src/task-manager.js'], function(TaskManager){
         _taskManager = new TaskManager();
 
         done();
+    });
+});
+
+beforeEach(function () {
+    store = {
+        'task-manager-tasks': [
+            {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
+            {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
+            {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" }
+        ]
+    };
+
+    spyOn(localStorage, 'getItem').and.callFake(function () {
+        return store['task-manager-tasks'];
+    });
+
+    spyOn(localStorage, 'setItem').and.callFake(function () {
+        return store['task-manager-tasks'] = [
+            {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
+            {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
+            {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
+            {"name": "Test Task #4", "date": "12/04/2012", "assigned": "John Doe" },
+            {"name": "Test Task #5", "date": "12/05/2012", "assigned": "John Doe" },
+            {"name": "Test Task #6", "date": "12/06/2012", "assigned": "John Doe" },
+            {"name": "Test Task #7", "date": "12/07/2012", "assigned": "John Doe" }
+        ];
     });
 });
 
@@ -26,5 +53,40 @@ describe("the TaskManager class", function () {
             {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
             {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" }
         ]);
+    });
+
+    it("should populate the tasks array with tasks if there are none in localStorage", function(){
+        store['task-manager-tasks'] = [];
+
+        _taskManager.loadTasks();
+
+        expect(_taskManager.getTasks()).toEqual([
+            {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
+            {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
+            {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
+            {"name": "Test Task #4", "date": "12/04/2012", "assigned": "John Doe" },
+            {"name": "Test Task #5", "date": "12/05/2012", "assigned": "John Doe" },
+            {"name": "Test Task #6", "date": "12/06/2012", "assigned": "John Doe" },
+            {"name": "Test Task #7", "date": "12/07/2012", "assigned": "John Doe" }
+        ]);
+    });
+
+    it('should persist a task to local storage', function(){
+        var task = {
+            name: 'Test Task #100',
+            date: '06/23/2015',
+            assigned: 'James Hines'
+        };
+
+        _taskManager.loadTasks();
+
+        _taskManager.saveTask(task);
+
+        expect(_taskManager.getTasks()).toEqual([
+            {"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
+            {"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
+            {"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
+            {"name": "Test Task #100", "date": "06/23/2015", "assigned": "James Hines" }
+        ])
     });
 });
